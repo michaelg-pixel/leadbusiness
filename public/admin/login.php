342a7ab28b2d93cbe050201f66ae4d7216261be1
michaelg-pixel <michael.gluska@gmail.com>
2025-12-09 17:24:31 +0100

    Fix: isPost() Helper-Funktion verwenden statt direkter $_SERVER Zugriff

--- public/admin/login.php
+++ public/admin/login.php
@@ -15,7 +15,7 @@ $error = '';
 $email = '';
 
 // Login verarbeiten
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+if (isPost()) {
     $email = sanitize($_POST['email'] ?? '');
     $password = $_POST['password'] ?? '';
     $remember = isset($_POST['remember']);
